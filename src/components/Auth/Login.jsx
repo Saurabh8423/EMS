54b0f52e211094272e2add07287e2036b7cea3ac
saurabh8423 <saurabhch9369@gmail.com>
2025-05-08 23:23:30 +0530

    create login page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Login() {
+const Login =() => {
+  
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    console.log("email is", email)
+    console.log("passwordis",password)
+  }
   return (
-    <div>
-      
+    <div className='flex h-screen w-screen items-center justify-center'>
+      <div className='border-2 rounded-xl border-emerald-600 p-20'>
+        <form 
+        onSubmit={(e)=>{
+          submitHandler(e)
+        }}
+        className='flex flex-col items-center justify-center'>
+          <input 
+          value={email}
+          onChange={(e)=>{
+            setEmail(e.target.value)
+          }}
+          required className='outline-none bg-transparent border-2 border-emerald-600 py-3 px-5 text-xl rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email' />
+
+          <input 
+          value={password}
+          onChange={(e)=>{
+            setPassword(e.target.value)
+          }}
+          required className='outline-none bg-transparent border-2 border-emerald-600 py-3 px-5 text-xl rounded-full mt-3 placeholder:text-gray-400' type="password" placeholder='Enter password' />
+          <button className='mt-5 text-white outline-none border-none bg-emerald-600 py-3 px-5 text-xl rounded-full placeholder:text-white'>Login</button>
+        </form>
+
+      </div> 
     </div>
   )
 }
--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function NewTask() {
   return (
     <div>
-      new Task
+      <>
+      <Login />
+      </>
     </div>
   )
 }
